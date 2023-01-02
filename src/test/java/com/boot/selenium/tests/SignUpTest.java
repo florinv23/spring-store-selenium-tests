a4c1e094ef5c3a48a329cc332e649038b5e39e62
florinv23 <100446582+florinv23@users.noreply.github.com>
2023-01-02 18:19:15 +0200

    Add failing sign-up test

--- src/test/java/com/boot/selenium/tests/SignUpTest.java
+++ src/test/java/com/boot/selenium/tests/SignUpTest.java
@@ -3,17 +3,24 @@ package com.boot.selenium.tests;
 import com.boot.selenium.util.ConfigurationsUtil;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.boot.selenium.util.Constants.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SignUpTest {
@@ -28,6 +35,9 @@ public class SignUpTest {
     private By confirmPassword = By.id("confirmPassword");
     private By signUpButton = By.id("signUpButton");
     private By signUpMsg = By.id("signUpMsg");
+    private By emailErrorMsg = By.id("email-helper-text");
+    private By phoneErrorMsg = By.id("phoneNumber-helper-text");
+    private By confirmPassErrorMsg = By.id("confirmPassword-helper-text");
 
     private static Properties properties;
 
@@ -57,6 +67,28 @@ public class SignUpTest {
         boolean isDisplayed = wait.until(
                 ExpectedConditions.textToBe(signUpMsg, "Succesfully signed Up! Please confirm your Email!"));
         assertTrue("Succesfully signed Up visible", isDisplayed);
+        driver.quit();
+    }
+
+    @Test
+    public void failingTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get(properties.getProperty(APPLICATION_URL));
+        driver.findElement(userButton).click();
+        driver.findElement(signUp).click();
+
+        driver.findElement(firstName).sendKeys("Test-FirstName");
+        driver.findElement(lastName).sendKeys("Test-LastName");
+        driver.findElement(email).sendKeys("test1234");
+        driver.findElement(phoneNumber).sendKeys("abcde");
+        driver.findElement(deliveryAddress).sendKeys("Test Street nr x");
+        driver.findElement(password).sendKeys("test1234");
+        driver.findElement(confirmPassword).sendKeys("test12345");
+        driver.findElement(signUpButton).submit();
+
+        assertEquals("Invalid Email!", driver.findElement(emailErrorMsg).getText());
+        assertEquals("Invalid Phone Number!", driver.findElement(phoneErrorMsg).getText());
+        assertEquals("Password does not match!", driver.findElement(confirmPassErrorMsg).getText());
         driver.quit();
     }
 }
